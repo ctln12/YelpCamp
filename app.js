91faa76a0e1f9b586340804589fe3edf4d24ee40
Cataline Baert <snuf_kin@yahoo.com>
2020-09-08 20:17:35 +0200

    add route for landing page and create views folder and ejs file for landing page

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const app = express();
 
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("landing");
+});
+
 app.listen(3000, () => {
   console.log("The Yelp Camp Server has started!");
 });
