ae08353c5b51fb2de122572e85ea8659d4625b7c
Cataline Baert <snuf_kin@yahoo.com>
2020-09-09 17:44:41 +0200

    add more campgrounds

--- app.js
+++ app.js
@@ -21,6 +21,36 @@ const campgrounds = [
     image:
       "https://images.unsplash.com/photo-1571863533956-01c88e79957e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80",
   },
+  {
+    name: "Salmon Creek",
+    image:
+      "https://media.wzzm13.com/assets/KUSA/images/c4403859-a976-48a1-8db2-4d180817128a/c4403859-a976-48a1-8db2-4d180817128a_750x422.jpg",
+  },
+  {
+    name: "Granite Hill",
+    image:
+      "https://www.nps.gov/grte/planyourvisit/images/JLCG_tents_Teewinot_2008_mattson_1.JPG",
+  },
+  {
+    name: "Mountain Goat's Rest",
+    image:
+      "https://images.unsplash.com/photo-1571863533956-01c88e79957e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80",
+  },
+  {
+    name: "Salmon Creek",
+    image:
+      "https://media.wzzm13.com/assets/KUSA/images/c4403859-a976-48a1-8db2-4d180817128a/c4403859-a976-48a1-8db2-4d180817128a_750x422.jpg",
+  },
+  {
+    name: "Granite Hill",
+    image:
+      "https://www.nps.gov/grte/planyourvisit/images/JLCG_tents_Teewinot_2008_mattson_1.JPG",
+  },
+  {
+    name: "Mountain Goat's Rest",
+    image:
+      "https://images.unsplash.com/photo-1571863533956-01c88e79957e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80",
+  },
 ];
 
 app.get("/", (req, res) => {
