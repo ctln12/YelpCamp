cd687d4db7c61950e0b9440815e80c9655eec4bf
Cataline Baert <snuf_kin@yahoo.com>
2020-09-15 10:11:14 +0200

    modify mongoose connect configuration

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const bodyParser = require("body-parser");
 const mongoose =require("mongoose");
 
-mongoose.connect("mongodb://localhost/yelp_camp", { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect("mongodb://localhost:27017/yelp_camp", { useNewUrlParser: true, useUnifiedTopology: true });
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
