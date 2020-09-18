497b71895e670c64c36b427e41f4779b14d551f5
Cataline Baert <snuf_kin@yahoo.com>
2020-09-18 11:45:06 +0200

    create models folder and move Blog model into it

---
+++ models/campground.js
@@ -0,0 +1,10 @@
+// SCHEMA SETUP
+const mongoose = require("mongoose");
+
+const campgroundSchema = new mongoose.Schema({
+name: String,
+image: String,
+description: String
+});
+
+module.exports = mongoose.model("Campground", campgroundSchema);
