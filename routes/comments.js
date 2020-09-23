52ab70520590a53fd9f642acd2a7ceae494f834d
Cataline Baert <snuf_kin@yahoo.com>
2020-09-23 09:18:54 +0200

    create comment ownership middleware, add it to edit, update, destroy routes and add show/hide condition to edit and delete buttons for comments in show template

--- routes/comments.js
+++ routes/comments.js
@@ -4,11 +4,32 @@ const Campground = require("../models/campground");
 const Comment = require("../models/comment");
 
 // Middleware
+// Authentication
 const isLoggedIn = (req, res, next) => {
   if (req.isAuthenticated()) {
     return next();
   }
   res.redirect("/login");
+};
+
+// Authorization
+const checkCommentOwnership = (req, res, next) => {
+  if (req.isAuthenticated()) {
+    Comment.findById(req.params.comment_id, (err, foundComment) => {
+      if (err) {
+        console.log(err);
+        res.redirect("back");
+      } else {
+        if (foundComment.author.id.equals(req.user._id)) {
+          next();
+        } else {
+          res.redirect("back");
+        }
+      }
+    });
+  } else {
+    res.redirect("back");
+  }
 };
 
 // Comments New
@@ -46,7 +67,7 @@ router.post("/", isLoggedIn, (req, res) => {
 });
 
 // Comments Edit
-router.get("/:comment_id/edit", (req, res) => {
+router.get("/:comment_id/edit", checkCommentOwnership, (req, res) => {
   Comment.findById(req.params.comment_id, (err, foundComment) => {
     if(err){
       res.redirect("back");
@@ -57,7 +78,7 @@ router.get("/:comment_id/edit", (req, res) => {
 });
 
 // Comments Update
-router.put("/:comment_id", (req, res) => {
+router.put("/:comment_id", checkCommentOwnership, (req, res) => {
   Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, (err, updatedComment) => {
     if(err){
       res.redirect("back");
@@ -68,7 +89,7 @@ router.put("/:comment_id", (req, res) => {
 });
 
 // Comments Destroy
-router.delete("/:comment_id", (req, res) => {
+router.delete("/:comment_id", checkCommentOwnership, (req, res) => {
   Comment.findByIdAndRemove(req.params.comment_id, (err) => {
     if(err){
       res.redirect("back");
