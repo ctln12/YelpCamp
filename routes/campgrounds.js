83c72fc9e7685bd9f5a96e92a3b86470bc119be9
Cataline Baert <snuf_kin@yahoo.com>
2020-09-23 13:55:12 +0200

    add price to campground model, add price to new campground route, add price input to new and edit campground forms, add campground price to show template

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -25,11 +25,12 @@ router.post("/", middleware.isLoggedIn, (req, res) => {
   const name = req.body.name;
   const image = req.body.image;
   const description = req.body.description;
+  const price = req.body.price;
   var author = {
     id: req.user._id,
     username: req.user.username
   }
-  const newCampground = { name: name, image: image, description: description, author: author };
+  const newCampground = { name: name, image: image, description: description, price: price, author: author };
   // Create a new campground and save to DB
   Campground.create(newCampground, (err, newlyCreated) => {
     if (err) {
