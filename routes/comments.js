3282e79e2bff930009acb1962a2f4b3a49371ac5
Cataline Baert <snuf_kin@yahoo.com>
2020-09-23 09:03:48 +0200

    add destroy routes and delete button in show template, change id to class and add it to both delete forms

--- routes/comments.js
+++ routes/comments.js
@@ -68,6 +68,14 @@ router.put("/:comment_id", (req, res) => {
 });
 
 // Comments Destroy
-
+router.delete("/:comment_id", (req, res) => {
+  Comment.findByIdAndRemove(req.params.comment_id, (err) => {
+    if(err){
+      res.redirect("back");
+    } else {
+      res.redirect("/campgrounds/" + req.params.id);
+    }
+  });
+});
 
 module.exports = router;
