6d6de55d8e15fe8260c21234788fef156edf9533
Cataline Baert <snuf_kin@yahoo.com>
2020-09-23 19:58:47 +0200

    add condition for connection to DB and port depending on development or production environment

--- app.js
+++ app.js
@@ -18,7 +18,16 @@ const campgroundRoutes = require("./routes/campgrounds");
 const commentRoutes = require("./routes/comments");
 const indexRoutes = require("./routes/index");
 
-mongoose.connect("mongodb://localhost:27017/yelp_camp", { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
+mongoose.connect(
+  process.env.MONGODB_URL || "mongodb://localhost:27017/yelp_camp", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+  }).then(() => {
+    console.log('Connected to DB');
+  }).catch(err => {
+    console.log("ERROR: ", err.message);
+  });
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
@@ -50,6 +59,6 @@ app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 app.use("/", indexRoutes);
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("The Yelp Camp Server has started!");
 });
